11ce3dd7f87c7ad3cc3de961ea38839b78418bb7
Bachir Çaoui <bachir.caoui@gmail.com>
2017-12-27 19:29:45 +0100

    PropTypes to components + tweaks

--- assets/js/components/Card.js
+++ assets/js/components/Card.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { DndItemTypes } from '../constants';
 import { DragSource } from 'react-dnd';
+import PropTypes from 'prop-types';
 
 const cardSource = {
   beginDrag(props) {
@@ -25,6 +26,11 @@ const Card = ({connectDragSource, isDragging, ...props}) => {
       {props.cards.find((card) => card.id === props.cardId).name}
     </div>
   )
+}
+
+Card.propTypes = {
+  cardId: PropTypes.string.isRequired,
+  cards: PropTypes.array.isRequired
 }
 
 export default DragSource(DndItemTypes.CARD, cardSource, collect)(Card);
--- assets/js/components/CardList.js
+++ assets/js/components/CardList.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 import Card from './Card';
 import { DndItemTypes } from '../constants';
 import { DropTarget } from 'react-dnd';
+import PropTypes from 'prop-types';
 
-const CardList = ({connectDropTarget, cardsByList, cardLists, cardListId, isOver, canDrop, ...props}) => {
+const CardList = ({connectDropTarget, cardsByList, cardLists, cardListId, ...props}) => {
 
   return connectDropTarget(
     <ul className="w3-ul" style={{listStyleType: 'none', height: '500px'}} key={props.cardListId}>
@@ -13,26 +14,11 @@ const CardList = ({connectDropTarget, cardsByList, cardLists, cardListId, isOver
       {cardsByList.find((list) => list.listId === cardListId).cardIds.map((card) => {
         return (
           <li key={card} style={{backgroundColor: 'yellow'}}>
-            <Card cardId={card} {...props} />
+            <Card cardId={card} cards={props.cards} />
           </li>
         );
       })}
     </ul>
-  );
-}
-
-const renderOverlay = (color) => {
-  return (
-    <div style={{
-      position: 'absolute',
-      top: 0,
-      left: 0,
-      height: '100%',
-      width: '100%',
-      zIndex: 1,
-      opacity: 0.5,
-      backgroundColor: color,
-    }} />
   );
 }
 
@@ -48,6 +34,10 @@ function collect(connect, monitor) {
     connectDropTarget: connect.dropTarget(),
     isOver: monitor.isOver()
   };
+}
+
+CardList.propTypes = {
+  cardListId: PropTypes.string.isRequired
 }
 
 export default DropTarget(DndItemTypes.CARD, listTarget, collect)(CardList);
