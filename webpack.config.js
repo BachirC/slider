09fd5d1a5175e121e4d354e7f5ae3e43a0ce798f
Bachir Çaoui <bachir.caoui@gmail.com>
2017-12-27 10:51:02 +0100

    Add loaders to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -7,4 +7,12 @@ module.exports = {
     path: path.resolve(__dirname, "./priv/static/js"),
     filename: "app.js"
   },
+
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /(node_modules)/,
+      loader: 'babel-loader'
+    }]
+  },
 };
