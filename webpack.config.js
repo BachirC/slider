a1e37b311731e565ce39cb979dbd09ea7301ad8f
Bachir Çaoui <bachir.caoui@gmail.com>
2017-12-27 10:38:31 +0100

    Remove duplicate declaration for presets (aleady in .babelrc)

--- webpack.config.js
+++ webpack.config.js
@@ -7,15 +7,4 @@ module.exports = {
     path: path.resolve(__dirname, "./priv/static/js"),
     filename: "app.js"
   },
-
-  module: {
-    loaders: [{
-      test: /\.js$/,
-      exclude: /(node_modules)/,
-      loader: 'babel-loader',
-      query: {
-        presets: ['env', 'react']
-      }
-    }]
-  },
 };
